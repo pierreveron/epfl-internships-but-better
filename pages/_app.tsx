424f2fb12e451616ca2baaedeb4bff656c07338b
Pierre Véron <pierre.veron@outlook.fr>
2023-03-01 14:30:57 +0100

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        /** Put your mantine theme override here */
-        colorScheme: "light",
-        primaryColor: "red",
-      }}
-    >
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+          primaryColor: "red",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      <Analytics />
+    </>
   );
 }
