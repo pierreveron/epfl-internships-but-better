69bb13a347dcadffb32ff2e7a2e5e9ae924f6682
Pierre Véron <pierre.veron@outlook.fr>
2023-03-03 13:36:29 +0100

    Clean modal opening logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import {
   showOnlyFavoriteAtom,
   showOnlyPositionsNotYetCompletedAtom,
 } from "@/atoms";
+import Footer from "@/components/Footer";
 import FormatsCheckboxes from "@/components/FormatsCheckboxes";
 import LocationsCheckbox from "@/components/LocationsCheckbox";
 import Table from "@/components/Table";
@@ -87,18 +88,12 @@ export default function Home({ data, dataDate }: Props) {
   const [hasModalBeenClosed, setHasModalBeenClosed] = useLocalStorage({
     key: "modal-closed",
     defaultValue: false,
+    getInitialValueInEffect: false,
   });
 
-  const [isModalOpened, { close: closeModal, open: openModal }] =
-    useDisclosure(false);
-
-  useEffect(() => {
-    if (!hasModalBeenClosed) {
-      openModal();
-    } else {
-      closeModal();
-    }
-  }, [hasModalBeenClosed, closeModal, openModal]);
+  const [isModalOpened, { close: closeModal }] = useDisclosure(
+    !hasModalBeenClosed
+  );
 
   const [
     modalPageCounter,
@@ -126,20 +121,78 @@ export default function Home({ data, dataDate }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {!hasModalBeenClosed && (
-        <Modal
-          opened={isModalOpened}
-          withCloseButton={false}
-          closeOnClickOutside={false}
-          closeOnEscape={false}
-          onClose={() => {}}
-          title={
-            <Title order={1}>
-              Welcome to{" "}
-              <Text c="red">
-                EPFL internships{" "}
+      <Modal
+        opened={isModalOpened}
+        withCloseButton={false}
+        closeOnClickOutside={false}
+        closeOnEscape={false}
+        onClose={() => {}}
+        title={
+          <Title order={1}>
+            Welcome to{" "}
+            <Text c="red">
+              EPFL internships{" "}
+              <Text
+                span
+                style={{
+                  background:
+                    "linear-gradient(to right, #fa5252 0%, #fd7e14 10%, #fa5252 20%)",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                  animation: "shine 2s infinite linear",
+                  backgroundSize: "200% 100%",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                BUT better
+              </Text>
+            </Text>
+          </Title>
+        }
+      >
+        <Stack>
+          <Divider />
+          <Title order={4}>
+            This website is a redesign concept of the Internships tab on{" "}
+            <span style={{ whiteSpace: "nowrap" }}>IS-Academia</span>.
+          </Title>
+          {modalPageCounter == 0 && (
+            <>
+              <Text>
+                The official interface tends to be not particularly
+                user-friendly, especially when it comes to filtering locations.
+                For example, if you want to look for internships in{" "}
+                <Text span fw={700}>
+                  Lausanne
+                </Text>
+                , you have to select{" "}
+                <Text span fw={700}>
+                  &quot;Lausanne&quot;
+                </Text>{" "}
+                but also{" "}
+                <Text span fw={700}>
+                  &quot;Lausanne/Geneva&quot;
+                </Text>
+                ,{" "}
+                <Text span fw={700}>
+                  &quot;Lausanne/Genève&quot;
+                </Text>{" "}
+                or{" "}
+                <Text span fw={700}>
+                  &quot;Lausanne/Zurich&quot;
+                </Text>{" "}
+                and so many more.
+              </Text>
+
+              <Text>
+                This concept notably aims to adress this particular issue. The
+                exact cities have been extracted from the original list of
+                locations using{" "}
                 <Text
                   span
+                  fw={700}
+                  c="red"
                   style={{
                     background:
                       "linear-gradient(to right, #fa5252 0%, #fd7e14 10%, #fa5252 20%)",
@@ -151,128 +204,68 @@ export default function Home({ data, dataDate }: Props) {
                     whiteSpace: "nowrap",
                   }}
                 >
-                  BUT better
+                  GPT-3
                 </Text>
+                .
               </Text>
-            </Title>
-          }
-        >
-          <Stack>
-            <Divider />
-            <Title order={4}>
-              This website is a redesign concept of the Internships tab on{" "}
-              <span style={{ whiteSpace: "nowrap" }}>IS-Academia</span>.
-            </Title>
-            {modalPageCounter == 0 && (
-              <>
-                <Text>
-                  The official interface tends to be not particularly
-                  user-friendly, especially when it comes to filtering
-                  locations. For example, if you want to look for internships in{" "}
-                  <Text span fw={700}>
-                    Lausanne
-                  </Text>
-                  , you have to select{" "}
-                  <Text span fw={700}>
-                    &quot;Lausanne&quot;
-                  </Text>{" "}
-                  but also{" "}
-                  <Text span fw={700}>
-                    &quot;Lausanne/Geneva&quot;
-                  </Text>
-                  ,{" "}
-                  <Text span fw={700}>
-                    &quot;Lausanne/Genève&quot;
-                  </Text>{" "}
-                  or{" "}
-                  <Text span fw={700}>
-                    &quot;Lausanne/Zurich&quot;
-                  </Text>{" "}
-                  and so many more.
+            </>
+          )}
+          {modalPageCounter > 0 && (
+            <>
+              <Text>
+                The data used here are internships targeting{" "}
+                <Text span fw={700}>
+                  Computer Science
+                </Text>{" "}
+                students.
+              </Text>
+              <Text>
+                If you are from this department, you can use this website to to
+                browse available internships assuming the data is{" "}
+                <Text span fw={700}>
+                  up-to-date
                 </Text>
-
-                <Text>
-                  This concept notably aims to adress this particular issue. The
-                  exact cities have been extracted from the original list of
-                  locations using{" "}
-                  <Text
-                    span
-                    fw={700}
-                    c="red"
-                    style={{
-                      background:
-                        "linear-gradient(to right, #fa5252 0%, #fd7e14 10%, #fa5252 20%)",
-                      backgroundClip: "text",
-                      WebkitBackgroundClip: "text",
-                      WebkitTextFillColor: "transparent",
-                      animation: "shine 2s infinite linear",
-                      backgroundSize: "200% 100%",
-                      whiteSpace: "nowrap",
-                    }}
-                  >
-                    GPT-3
-                  </Text>
-                  .
-                </Text>
-              </>
-            )}
-            {modalPageCounter > 0 && (
-              <>
-                <Text>
-                  The data used here are internships targeting{" "}
-                  <Text span fw={700}>
-                    Computer Science
-                  </Text>{" "}
-                  students.
-                </Text>
-                <Text>
-                  If you are from this department, you can use this website to
-                  to browse available internships assuming the data is{" "}
-                  <Text span fw={700}>
-                    up-to-date
-                  </Text>
-                  . Current data dates from the{" "}
-                  <Text
-                    span
-                    fw={700}
-                    style={{
-                      background:
-                        "linear-gradient(to right, #fa5252 0%, #fd7e14 10%, #fa5252 20%)",
-                      backgroundClip: "text",
-                      WebkitBackgroundClip: "text",
-                      WebkitTextFillColor: "transparent",
-                      animation: "shine 2s infinite linear",
-                      backgroundSize: "200% 100%",
-                      whiteSpace: "nowrap",
-                    }}
-                  >
-                    {dataDate}
-                  </Text>
-                  .
-                </Text>
-                <Alert
-                  icon={<IconAlertCircle size={16} />}
-                  title="This website don't fully replace IS-Academia"
-                  color="red"
+                . Current data dates from the{" "}
+                <Text
+                  span
+                  fw={700}
+                  style={{
+                    background:
+                      "linear-gradient(to right, #fa5252 0%, #fd7e14 10%, #fa5252 20%)",
+                    backgroundClip: "text",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                    animation: "shine 2s infinite linear",
+                    backgroundSize: "200% 100%",
+                    whiteSpace: "nowrap",
+                  }}
                 >
-                  Once you identify an internship of interest, please take note
-                  of its number and look for it on IS-Academia, where you can
-                  submit your application.
-                </Alert>
-              </>
-            )}
+                  {dataDate}
+                </Text>
+                .
+              </Text>
+              <Alert
+                icon={<IconAlertCircle size={16} />}
+                title="This website don't fully replace IS-Academia"
+                color="red"
+              >
+                Once you identify an internship of interest, please take note of
+                its number and look for it on IS-Academia, where you can submit
+                your application.
+              </Alert>
+            </>
+          )}
 
-            <Group>
-              {modalPageCounter > 0 && (
-                <Button onClick={decrementModalPageCounter}>Back</Button>
-              )}
-              <Button ml="auto" onClick={incrementModalPageCounter}>
-                {modalPageCounter == 0 ? "Next" : "Done"}
-              </Button>
-            </Group>
-          </Stack>
-        </Modal>
-      )}
+          <Group>
+            {modalPageCounter > 0 && (
+              <Button onClick={decrementModalPageCounter}>Back</Button>
+            )}
+            <Button ml="auto" onClick={incrementModalPageCounter}>
+              {modalPageCounter == 0 ? "Next" : "Done"}
+            </Button>
+          </Group>
+        </Stack>
+      </Modal>
       <Stack style={{ height: "100vh" }} p="xl">
         <Group>
           <Switch
@@ -382,7 +375,6 @@ export default function Home({ data, dataDate }: Props) {
 import { promises as fs } from "fs";
 import path from "path";
 
-import Footer from "@/components/Footer";
 import { GetStaticProps } from "next";
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
