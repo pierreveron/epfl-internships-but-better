e137306e7a13ea7f0e89a9ddeb18190b2ccc6fb3
Pierre Véron <pierre.veron@outlook.fr>
2023-10-04 23:14:46 +0200

    refactor: tailwind content

--- chrome-extension/tailwind.config.js
+++ chrome-extension/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   prefix: 'tw-',
-  content: ['./input.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./input.html', './src/**/*.tsx'],
   theme: {
     extend: {},
   },
