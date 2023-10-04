ff7c9190960913c76c8ee62bd023d9051d0e89a8
Pierre Véron <pierre.veron@outlook.fr>
2023-10-04 18:01:11 +0200

    feat: refactor src/

--- chrome-extension/src/popup/index.tsx
+++ chrome-extension/src/popup/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Popup from './Popup.tsx'
+import '../styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('extension-root')!).render(
   <React.StrictMode>
-    <div className="tw-max-w-7xl tw-p-8 tw-text-center tw-min-w-fit">Test</div>
-    <App />
+    <Popup />
   </React.StrictMode>,
 )
