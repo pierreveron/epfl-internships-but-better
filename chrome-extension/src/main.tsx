6ab38cd61d4f5860da1a868d7491bcf879eb84b5
Pierre Véron <pierre.veron@outlook.fr>
2023-10-04 17:41:17 +0200

    feat: replace default css by tailwind

--- chrome-extension/src/main.tsx
+++ chrome-extension/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('extension-root')!).render(
   <React.StrictMode>
+    <div className="tw-max-w-7xl tw-p-8 tw-text-center tw-min-w-fit">Test</div>
     <App />
   </React.StrictMode>,
 )
