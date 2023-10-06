fbf29d3a619b8b5be5de56ccc72de37f1ba7cca1
Pierre Véron <pierre.veron@outlook.fr>
2023-10-06 15:26:09 +0200

    chore: add new npm scripts to watch

--- chrome-extension/vite.config.ts
+++ chrome-extension/vite.config.ts
@@ -6,4 +6,7 @@ import manifest from './manifest.json'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  build: {
+    emptyOutDir: false,
+  },
 })
