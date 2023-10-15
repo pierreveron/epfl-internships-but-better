8f8bd18a44443669422a350ac0cc666236bad419
Pierre Véron <pierre.veron@outlook.fr>
2023-10-15 21:20:47 +0200

    feat: disable content

--- chrome-extension/src/content/Content.tsx
+++ chrome-extension/src/content/Content.tsx
@@ -1,20 +1,4 @@
-import { useEffect, useState } from 'react'
-import { scrapeJobs } from '../utils/scraping'
-
 export default function Content() {
-  const [progessText, setProgressText] = useState('Scraping...')
-
-  useEffect(() => {
-    scrapeJobs((text) => setProgressText(text)).then((jobOffers) => {
-      console.log('Job offers', jobOffers)
-      /**
-       * Job offers are send to the background script to be saved in the storage of the extension
-       * @file chrome-extension/src/background/serviceWorker.ts
-       */
-      chrome.runtime.sendMessage({ jobOffers })
-    })
-  }, [])
-
   return (
     <div
       className="tw-fixed tw-inset-0 tw-bg-gray-500 tw-bg-opacity-75 tw-transition-opacity tw-z-[1000]"
@@ -22,7 +6,7 @@ export default function Content() {
       role="dialog"
       aria-modal="true"
     >
-      <p className="tw-text-4xl tw-text-white">{progessText}</p>
+      <p className="tw-text-4xl tw-text-white">Scraping...</p>
     </div>
   )
 }
