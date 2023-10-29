bc7f7ad0f07bd6f23e5632bb948b3a9d689a97b2
Pierre Véron <pierre.veron@outlook.fr>
2023-10-29 15:19:26 +0100

    refactor: extract ActionBar

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -3,14 +3,9 @@ import {
   lengthAtom,
   locationsAtom,
   nbCitiesSelectedAtom,
-  showOnlyFavoritesAtom,
-  showOnlyPositionsNotYetCompletedAtom,
 } from "@/atoms";
-import CompanySelect from "@/components/CompanySelect";
+import ActionBar from "@/components/ActionBar";
 import Footer from "@/components/Footer";
-import FormatsSegmentedControl from "@/components/FormatsSegmentedControl";
-import LengthsCheckboxes from "@/components/LengthsCheckboxes";
-import LocationsCheckbox from "@/components/LocationsCheckbox";
 import Table from "@/components/Table";
 import WelcomingModal from "@/components/WelcomingModal";
 
@@ -20,27 +15,8 @@ import {
   SelectableFormat,
   SelectableLength,
 } from "@/types";
-import {
-  Alert,
-  Button,
-  Divider,
-  Group,
-  HoverCard,
-  Modal,
-  Popover,
-  Stack,
-  Switch,
-  Text,
-  ThemeIcon,
-  Title,
-} from "@mantine/core";
-import { useCounter, useDisclosure, useLocalStorage } from "@mantine/hooks";
-import {
-  IconAlertCircle,
-  IconChevronDown,
-  IconInfoCircle,
-} from "@tabler/icons";
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { Stack } from "@mantine/core";
+import { useAtomValue, useSetAtom } from "jotai";
 import Head from "next/head";
 import { useEffect, useMemo, useState } from "react";
 
@@ -109,16 +85,9 @@ export default function Home() {
   }, [locations]);
 
   const setSelectableFormats = useSetAtom(formatAtom);
-  const [selectableLengths, setSelectableLengths] = useAtom(lengthAtom);
-  const [selectableLocations, setSelectableLocations] = useAtom(locationsAtom);
+  const setSelectableLengths = useSetAtom(lengthAtom);
+  const setSelectableLocations = useSetAtom(locationsAtom);
   const nbCitiesSelected = useAtomValue(nbCitiesSelectedAtom);
-  const [
-    showOnlyPositionsNotYetCompleted,
-    setShowOnlyPositionsNotYetCompleted,
-  ] = useAtom(showOnlyPositionsNotYetCompletedAtom);
-  const [showOnlyFavorites, setShowOnlyFavorite] = useAtom(
-    showOnlyFavoritesAtom
-  );
 
   useEffect(() => {
     setSelectableFormats(
@@ -161,117 +130,12 @@ export default function Home() {
       <WelcomingModal dataDate={dataDate} />
 
       <Stack style={{ height: "100vh" }} p="xl">
-        <Group position="apart">
-          <Group>
-            <FormatsSegmentedControl />
+        <ActionBar
+          nbCitiesSelected={nbCitiesSelected}
+          companies={companies}
+          dataDate={dataDate}
+        />
 
-            <Popover position="bottom-start" shadow="md">
-              <Popover.Target>
-                <Button
-                  rightIcon={<IconChevronDown size={18} />}
-                  variant="outline"
-                >
-                  Select locations
-                </Button>
-              </Popover.Target>
-              <Popover.Dropdown style={{ maxHeight: 300, overflowY: "scroll" }}>
-                <Stack spacing="xs">
-                  <Button
-                    variant="subtle"
-                    disabled={nbCitiesSelected === 0}
-                    onClick={() =>
-                      setSelectableLocations((locations) => {
-                        Object.keys(locations).forEach((country) => {
-                          locations[country].forEach((city) => {
-                            city.selected = false;
-                          });
-                        });
-                        return { ...locations };
-                      })
-                    }
-                  >
-                    Reset
-                  </Button>
-                  {Object.keys(selectableLocations).map((country) => (
-                    <LocationsCheckbox key={country} country={country} />
-                  ))}
-                </Stack>
-              </Popover.Dropdown>
-            </Popover>
-
-            <Popover position="bottom-start" shadow="md">
-              <Popover.Target>
-                <Button
-                  rightIcon={<IconChevronDown size={18} />}
-                  variant="outline"
-                >
-                  Select lengths
-                </Button>
-              </Popover.Target>
-              <Popover.Dropdown>
-                <Stack spacing="xs">
-                  <Button
-                    variant="subtle"
-                    disabled={
-                      selectableLengths.filter((v) => v.selected).length === 0
-                    }
-                    onClick={() =>
-                      setSelectableLengths((lengths) => {
-                        return lengths.map((f) => ({ ...f, selected: false }));
-                      })
-                    }
-                  >
-                    Reset
-                  </Button>
-                  <LengthsCheckboxes />
-                </Stack>
-              </Popover.Dropdown>
-            </Popover>
-
-            <CompanySelect companies={companies} />
-          </Group>
-
-          <Group>
-            <Switch
-              styles={{
-                label: { maxWidth: 200 },
-                body: { alignItems: "center" },
-              }}
-              label="Show only offers with less candidates than places"
-              checked={showOnlyPositionsNotYetCompleted}
-              onChange={(event) =>
-                setShowOnlyPositionsNotYetCompleted(event.currentTarget.checked)
-              }
-            />
-            <Switch
-              label="Show only favorites"
-              checked={showOnlyFavorites}
-              onChange={(event) =>
-                setShowOnlyFavorite(event.currentTarget.checked)
-              }
-            />
-          </Group>
-
-          <Group spacing="xs">
-            <Text c="dimmed">Last data update: {dataDate}</Text>
-            <HoverCard width={280} shadow="md">
-              <HoverCard.Target>
-                <ThemeIcon
-                  variant="light"
-                  color="gray"
-                  style={{ cursor: "pointer" }}
-                >
-                  <IconInfoCircle size={18} />
-                </ThemeIcon>
-              </HoverCard.Target>
-              <HoverCard.Dropdown>
-                <Text size="sm">
-                  Data could not be up-to-date as it has to be changed manually.
-                </Text>
-              </HoverCard.Dropdown>
-            </HoverCard>
-          </Group>
-        </Group>
         <Table data={data} />
         <Footer />
       </Stack>
