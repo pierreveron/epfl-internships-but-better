6ad5f90f4769db441baa0a726eafb7cd4bcdacf6
Pierre Véron <pierre.veron@outlook.fr>
2023-11-01 16:13:55 +0100

    feat: add error boundary to website

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import { Anchor, MantineProvider } from "@mantine/core";
 import { Analytics } from "@vercel/analytics/react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   /**
@@ -32,7 +33,32 @@ export default function App({ Component, pageProps }: AppProps) {
           primaryColor: "red",
         }}
       >
-        <Component {...pageProps} />
+        <ErrorBoundary
+          fallback={
+            <div className="tw-h-screen tw-flex tw-flex-col tw-justify-center tw-items-center">
+              <h3 className="tw-text-2xl tw-text-[#868e96] tw-font-semibold">
+                Oups... <span className="tw-text-3xl">🙇‍♂️</span>
+              </h3>
+              <h3 className="tw-text-xl tw-text-[#868e96] tw-font-medium">
+                Something went wrong while loading the data
+              </h3>
+              <p className="tw-text-lg tw-text-[#868e96]">
+                Please try again (we never know{" "}
+                <span className="tw-text-xl">🤷‍♂️</span>) & contact Pierre Véron
+                on{" "}
+                <Anchor
+                  href="https://www.linkedin.com/in/pierre-veron/"
+                  target="_blank"
+                >
+                  Linkedin
+                </Anchor>{" "}
+                or by <Anchor href="mailto:pierre.veron@epfl.ch">email</Anchor>
+              </p>
+            </div>
+          }
+        >
+          <Component {...pageProps} />
+        </ErrorBoundary>
       </MantineProvider>
       <Analytics />
     </>
