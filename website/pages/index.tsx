64ddfa7b7b323899e0f52ef2e320638d6c1d52fe
Pierre Véron <pierre.veron@outlook.fr>
2023-11-11 17:59:39 +0100

    feat: wrap app with appshell and draft aside

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -12,11 +12,12 @@ import WelcomingModal from "@/components/WelcomingModal";
 
 import { SelectableCity, SelectableLength } from "@/types";
 import { abortFormatting, formatLocations } from "@/utils/locations-formatting";
-import { Anchor, Stack } from "@mantine/core";
+import { Anchor, Stack, AppShell, Skeleton } from "@mantine/core";
 import { useAtomValue, useSetAtom } from "jotai";
 import Head from "next/head";
 import { useEffect, useMemo, useState } from "react";
 import { Offer, OfferWithLocationToBeFormatted } from "../../types";
+import { useDisclosure } from "@mantine/hooks";
 
 const NOT_SPECIFIED = "Not specified";
 
@@ -159,6 +160,9 @@ export default function Home() {
     setSelectableLocations(citiesByCountry);
   }, [citiesByCountry, setSelectableLocations]);
 
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(false);
+
   return (
     <>
       <Head>
@@ -173,39 +177,63 @@ export default function Home() {
 
       <WelcomingModal />
 
-      <Stack style={{ height: "100vh" }} p="xl">
-        <ActionBar
-          nbCitiesSelected={nbCitiesSelected}
-          companies={companies}
-          dataDate={dataDate}
-        />
+      <AppShell
+        // style={{ height: "100vh" }}
+        aside={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        }}
+        padding="xl"
+      >
+        <AppShell.Aside p="xl">
+          Aside
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShell.Aside>
+        <AppShell.Main>
+          <Stack
+          // style={{ height: "100vh" }}
+          >
+            <ActionBar
+              nbCitiesSelected={nbCitiesSelected}
+              companies={companies}
+              dataDate={dataDate}
+            />
 
-        {isError ? (
-          <div className="tw-h-full tw-flex tw-flex-col tw-justify-center tw-items-center">
-            <h3 className="tw-text-2xl tw-text-[#868e96] tw-font-semibold">
-              Oups... <span className="tw-text-3xl">🙇‍♂️</span>
-            </h3>
-            <h3 className="tw-text-xl tw-text-[#868e96] tw-font-medium">
-              Something went wrong while loading the data
-            </h3>
-            <p className="tw-text-lg tw-text-[#868e96]">
-              Please try again (we never know{" "}
-              <span className="tw-text-xl">🤷‍♂️</span>) & contact Pierre Véron on{" "}
-              <Anchor
-                href="https://www.linkedin.com/in/pierre-veron/"
-                target="_blank"
-              >
-                Linkedin
-              </Anchor>{" "}
-              or by <Anchor href="mailto:pierre.veron@epfl.ch">email</Anchor>
-            </p>
-          </div>
-        ) : (
-          <Table data={data} />
-        )}
+            {isError ? (
+              <div className="tw-h-full tw-flex tw-flex-col tw-justify-center tw-items-center">
+                <h3 className="tw-text-2xl tw-text-[#868e96] tw-font-semibold">
+                  Oups... <span className="tw-text-3xl">🙇‍♂️</span>
+                </h3>
+                <h3 className="tw-text-xl tw-text-[#868e96] tw-font-medium">
+                  Something went wrong while loading the data
+                </h3>
+                <p className="tw-text-lg tw-text-[#868e96]">
+                  Please try again (we never know{" "}
+                  <span className="tw-text-xl">🤷‍♂️</span>) & contact Pierre Véron
+                  on{" "}
+                  <Anchor
+                    href="https://www.linkedin.com/in/pierre-veron/"
+                    target="_blank"
+                  >
+                    Linkedin
+                  </Anchor>{" "}
+                  or by{" "}
+                  <Anchor href="mailto:pierre.veron@epfl.ch">email</Anchor>
+                </p>
+              </div>
+            ) : (
+              <Table data={data} />
+            )}
 
-        <Footer />
-      </Stack>
+            <Footer />
+          </Stack>
+        </AppShell.Main>
+      </AppShell>
     </>
   );
 }
