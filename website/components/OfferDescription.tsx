82c1d755e337f2486b0f03424b76948b972f49b9
Pierre Véron <pierre.veron@outlook.fr>
2023-11-13 12:29:05 +0100

    draft: marked favorite in aside

--- website/components/OfferDescription.tsx
+++ website/components/OfferDescription.tsx
@@ -1,9 +1,19 @@
 import { asideOfferAtom } from "@/atoms";
 import { useAtomValue } from "jotai";
 import LocationDotIcon from "./icons/LocationDotIcon";
+import HeartIcon from "./HeartIcon";
+import { useLocalStorage } from "@mantine/hooks";
 
 export default function OfferDescription() {
   const asideOffer = useAtomValue(asideOfferAtom);
+
+  // const [favoriteInternships, setFavoriteInternships] = useLocalStorage({
+  //   key: "favorite-internships",
+  //   getInitialValueInEffect: true,
+  //   defaultValue: [] as string[],
+  // });
+
+  // const isFavorite = favoriteInternships.includes(asideOffer?.number ?? "");
 
   return (
     <>
@@ -11,6 +21,26 @@ export default function OfferDescription() {
       <p className="tw-text-lg tw-font-medium tw-italic">
         {asideOffer?.company}
       </p>
+
+      <div>
+        <HeartIcon
+          checked={asideOffer?.favorite ?? false}
+          // onClick={() => {
+          //   let checked = !isFavorite;
+          //   let number = asideOffer?.number ?? "";
+          //   setFavoriteInternships((favorites) => {
+          //     if (checked) {
+          //       if (!favorites.includes(number)) {
+          //         return [...favorites, number];
+          //       }
+          //       return favorites;
+          //     } else {
+          //       return favorites.filter((f) => f !== number);
+          //     }
+          //   });
+          // }}
+        />
+      </div>
 
       <div className="tw-flex tw-flex-row tw-gap-4">
         <LocationDotIcon className="tw-w-4 tw-h-4 tw-text-gray-500" />
