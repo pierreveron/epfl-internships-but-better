3f6a69d6a6150eb8168296717fdf22575595f03b
Pierre Véron <pierre.veron@outlook.fr>
2023-11-17 16:43:37 +0100

    feat: add api key check

--- server/main.py
+++ server/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, Body, Request
-from fastapi.middleware.cors import CORSMiddleware
-from typing import List, Annotated
-
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
+from typing import Annotated, List
 
 from clean_bad_locations_openai import clean_locations as clean_locations_openai
 from clean_salaries_openai import clean_salaries as clean_salairies_openai
+from fastapi import Body, FastAPI, HTTPException, Request, Security, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import APIKeyHeader
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
 app = FastAPI()
 limiter = Limiter(key_func=get_remote_address)
@@ -27,10 +27,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+api_keys = ["my_api_key"]
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header in api_keys:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+
 
 @app.post("/clean-locations")
 @limiter.limit("3/day")
-async def clean_locations(request: Request, locations: Annotated[List[str], Body()]):
+async def clean_locations(
+    request: Request,
+    locations: Annotated[List[str], Body()],
+    api_key: str = Security(get_api_key),
+):
     if len(locations) > 500:
         return {"error": "Too many locations"}
     return clean_locations_openai(locations)
@@ -38,7 +55,11 @@ async def clean_locations(request: Request, locations: Annotated[List[str], Body
 
 @app.post("/clean-salaries")
 @limiter.limit("3/day")
-async def clean_salaries(request: Request, salaries: Annotated[List[str], Body()]):
+async def clean_salaries(
+    request: Request,
+    salaries: Annotated[List[str], Body()],
+    api_key: str = Security(get_api_key),
+):
     if len(salaries) > 700:
         return {"error": "Too many salaries"}
     return clean_salairies_openai(salaries)
