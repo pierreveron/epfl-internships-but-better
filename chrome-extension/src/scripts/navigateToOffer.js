ba6703ce22739b237b4df2eb83bf51def7b70f76
Pierre Véron <pierre.veron@outlook.fr>
2023-11-18 12:37:26 +0100

    refactor: navigateToOffer

--- chrome-extension/src/scripts/navigateToOffer.js
+++ chrome-extension/src/scripts/navigateToOffer.js
@@ -1,32 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function navigateToRegister(offerId) {
-  // eslint-disable-next-line no-undef
-  if (!prtl && !prtl.synchronize) {
-    return
-  }
-
-  var i = 0
-
-  while (i < 10) {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    if (document.querySelectorAll('tr[id]').length !== 0) {
-      break
-    }
-    i++
-  }
-
-  // eslint-disable-next-line no-undef
-  prtl.synchronize('ww_i_stageview=' + offerId)
-  // eslint-disable-next-line no-undef
-  prtl.synchronize('ww_i_stageinscr=' + offerId)
-}
-
 async function navigateToView(offerId) {
   // eslint-disable-next-line no-undef
   if (!prtl && !prtl.synchronize) {
-    return
+    return false
   }
 
+  await waitLoad()
+
+  // eslint-disable-next-line no-undef
+  prtl.synchronize('ww_i_stageview=' + offerId)
+  return true
+}
+
+async function navigateToRegister(offerId) {
+  if (await navigateToView(offerId)) {
+    // eslint-disable-next-line no-undef
+    prtl.synchronize('ww_i_stageinscr=' + offerId)
+    return true
+  }
+  return false
+}
+
+async function waitLoad() {
   var i = 0
 
   while (i < 10) {
@@ -36,9 +30,6 @@ async function navigateToView(offerId) {
     }
     i++
   }
-
-  // eslint-disable-next-line no-undef
-  prtl.synchronize('ww_i_stageview=' + offerId)
 }
 
 if (document.querySelector('script#navigateToRegister')) {
