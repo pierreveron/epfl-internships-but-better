1bd5c030ffd6ff13bb031c66ac6c203d03cad8d1
Pierre Véron <pierre.veron@outlook.fr>
2023-11-18 12:29:03 +0100

    refactor: isascript

--- chrome-extension/src/scripts/isaContentScript.ts
+++ chrome-extension/src/scripts/isaContentScript.ts
@@ -1,10 +1,10 @@
 // https://stackoverflow.com/questions/9515704/access-variables-and-functions-defined-in-page-context-using-a-content-script
 chrome.runtime.onMessage.addListener(async function (request) {
-  if (request.type == 'register' && request.offerId) {
+  const createScript = (id: string) => {
     const s = document.createElement('script')
     s.textContent = request.offerId
     s.src = chrome.runtime.getURL('src/scripts/navigateToOffer.js')
-    s.id = 'navigateToRegister'
+    s.id = id
     s.onload = function () {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
@@ -14,17 +14,11 @@ chrome.runtime.onMessage.addListener(async function (request) {
     ;(document.head || document.documentElement).appendChild(s)
   }
 
-  if (request.type == 'view' && request.offerId) {
-    const s = document.createElement('script')
-    s.textContent = request.offerId
-    s.src = chrome.runtime.getURL('src/scripts/navigateToOffer.js')
-    s.id = 'navigateToView'
-    s.onload = function () {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      this.remove()
-    }
-    // see also "Dynamic values in the injected code" section in this answer
-    ;(document.head || document.documentElement).appendChild(s)
+  if (request.type === 'register' && request.offerId) {
+    createScript('navigateToRegister')
+  }
+
+  if (request.type === 'view' && request.offerId) {
+    createScript('navigateToView')
   }
 })
