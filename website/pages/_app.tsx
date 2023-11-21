1dbb96aac38e26e26da41ce73a6e3fb844ee6bea
Pierre Véron <pierre.veron@outlook.fr>
2023-11-21 21:28:06 +0100

    refactor: simplify key verification

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -2,23 +2,14 @@ import "@/styles/globals.css";
 import "@mantine/core/styles.css";
 import "mantine-datatable/styles.css";
 
-import UnlockIcon from "@/components/icons/UnlockIcon";
 import { API_URL } from "@/utils/constants";
-import {
-  Anchor,
-  Button,
-  Group,
-  MantineProvider,
-  Modal,
-  TextInput,
-  createTheme,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { useLocalStorage } from "@mantine/hooks";
+import { Anchor, MantineProvider, createTheme } from "@mantine/core";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
+import { redirect, usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { useLocalStorage } from "@mantine/hooks";
 
 export default function App({ Component, pageProps }: AppProps) {
   /**
@@ -43,20 +34,18 @@ export default function App({ Component, pageProps }: AppProps) {
   // Then redefine the old console
   console = newConsole;
 
-  const [apiKey, setApiKey] = useLocalStorage({
+  const [apiKey, _] = useLocalStorage({
     key: "apiKey",
     defaultValue: "",
     getInitialValueInEffect: false,
   });
 
-  const [isApiKeyValid, setIsApiKeyValid] = useState<boolean>(true);
-
-  const [isFetching, setIsFetching] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
-    if (apiKey == "") {
-      setIsApiKeyValid(false);
-      return;
+    if (apiKey === "") {
+      router.push("/welcome");
     }
 
     fetch(API_URL, {
@@ -66,16 +55,13 @@ export default function App({ Component, pageProps }: AppProps) {
       },
     }).then((res) => {
       if (res.ok) {
-        setIsApiKeyValid(true);
+        // Redirect to / if the user is on /welcome
+        if (pathname === "/welcome") router.push("/");
       } else {
-        setIsApiKeyValid(false);
+        if (pathname !== "/welcome") router.push("/welcome");
       }
     });
   }, [apiKey]);
-
-  const form = useForm({
-    initialValues: { key: "" },
-  });
 
   return (
     <>
@@ -104,112 +90,7 @@ export default function App({ Component, pageProps }: AppProps) {
             </div>
           }
         >
-          <Modal
-            opened={!isApiKeyValid}
-            onClose={() => {}}
-            fullScreen
-            withCloseButton={false}
-            radius={0}
-            transitionProps={{ transition: "fade", duration: 200 }}
-            styles={{
-              body: { height: "100%" },
-            }}
-          >
-            <div className="tw-mx-auto tw-my-auto tw-w-fit tw-h-full tw-flex tw-flex-col tw-justify-center tw-gap-4">
-              <div className="sm:tw-w-min tw-space-y-4">
-                <div className="tw-space-y-1">
-                  <h1 className="tw-text-5xl tw-text-center tw-font-semibold tw-text-gray-900 sm:tw-whitespace-nowrap">
-                    <span className="tw-text-[#fa5252]">EPFL</span> internships
-                    but <span className="shine">better</span>
-                  </h1>
-
-                  <p className="tw-text-gray-900 tw-text-base4">
-                    This website is a redesign concept of the job board on{" "}
-                    <span className="tw-font-semibold">IS-Academia</span>, the
-                    study management tool of EPFL.
-                  </p>
-                </div>
-
-                <div className="tw-space-y-1">
-                  <p className="tw-text-gray-900 tw-text-base">
-                    In order to use this it, you need to have a secret key. If
-                    you don&apos;t have one, please contact{" "}
-                    <Anchor
-                      href="https://www.linkedin.com/in/pierre-veron/"
-                      target="_blank"
-                      underline="never"
-                    >
-                      Pierre Véron
-                    </Anchor>
-                    . Otherwise, please enter it below.
-                  </p>
-
-                  <p className="tw-text-gray-900 tw-text-base">
-                    Also, take note that the data is{" "}
-                    <span className="tw-font-semibold">
-                      not updated in real time
-                    </span>
-                    : you need to use the extension every time you want to
-                    update the data.
-                  </p>
-
-                  <p className="tw-text-gray-900 tw-text-base">
-                    Finally,{" "}
-                    <span className="tw-font-semibold">save this website</span>{" "}
-                    to your favorites and{" "}
-                    <span className="shine tw-font-bold">good luck</span> in
-                    your search!
-                  </p>
-                </div>
-              </div>
-
-              <form
-                onSubmit={form.onSubmit(({ key }) => {
-                  setIsFetching(true);
-
-                  fetch(API_URL, {
-                    method: "GET",
-                    headers: {
-                      "X-API-Key": key,
-                    },
-                  })
-                    .then((res) => {
-                      if (res.ok) {
-                        setApiKey(key);
-                        setIsApiKeyValid(true);
-                      } else {
-                        form.setFieldError("key", "Invalid key");
-                      }
-                    })
-                    .catch(() => {
-                      form.setFieldError("key", "An error occured...");
-                    })
-                    .finally(() => setIsFetching(false));
-                })}
-              >
-                <TextInput
-                  data-autofocus
-                  label="Secret key"
-                  placeholder="Enter your secret key"
-                  {...form.getInputProps("key")}
-                />
-                <Group justify="flex-end" mt="sm">
-                  <Button
-                    className="tw-mt-4"
-                    type="submit"
-                    loading={isFetching}
-                    disabled={form.getInputProps("key").value == ""}
-                    rightSection={
-                      <UnlockIcon className="tw-w-4 tw-h-4 tw-fill-current" />
-                    }
-                  >
-                    Enter
-                  </Button>
-                </Group>
-              </form>
-            </div>
-          </Modal>
-          {isApiKeyValid && <Component {...pageProps} />}
+          <Component {...pageProps} />
         </ErrorBoundary>
       </MantineProvider>
       <Analytics />
