6aefd3a3925998d8b3187e311848c9bc4f9df530
Pierre Véron <pierre.veron@outlook.fr>
2024-03-17 19:16:59 +0100

    feat: change website link position

--- website/components/OfferDescription.tsx
+++ website/components/OfferDescription.tsx
@@ -53,33 +53,39 @@ export default function OfferDescription() {
     <div className="tw-pb-8">
       <p className="tw-text-gray-500 tw-text-sm">
         {/* <span className="tw-font-medium">Offer number:</span>{" "} */}
-        {asideOffer?.number}
+        {asideOffer.number}
       </p>
 
       <div className="tw-mb-4">
         <h2 className="tw-text-2xl tw-font-bold">{asideOffer?.title}</h2>
         <p className="tw-text-lg tw-font-medium tw-italic">
-          {asideOffer?.company}
-        </p>
-        <div className="tw-flex tw-flex-col">
-          {asideOffer?.companyInfo.website &&
-            asideOffer?.companyInfo.website
+          {asideOffer.company}
+          {asideOffer.companyInfo.website &&
+            asideOffer.companyInfo.website
               .split(";")
               .map((t) => t.trim())
               .map((website, index) => (
-                <a
-                  key={index}
-                  className="tw-text-blue-500 tw-text-sm tw-underline"
-                  href={
-                    website.startsWith("http") ? website : `http://${website}`
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {website}
-                </a>
+                <span className="before:tw-content-['_·_']">
+                  <a
+                    key={index}
+                    className="tw-no-underline hover:tw-fill-red-500 tw-transition-colors"
+                    href={
+                      website.startsWith("http") ? website : `http://${website}`
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                      className="tw-w-[0.8rem]"
+                    >
+                      <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+                    </svg>
+                  </a>
+                </span>
               ))}
-        </div>
+        </p>
       </div>
 
       <div className="tw-mb-4">
