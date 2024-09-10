35677a174e99305e46fdd8622b1c6c115708db96
Pierre Véron <pierre.veron@outlook.fr>
2024-09-10 17:01:02 +0200

    feat: init content script

---
+++ chrome-extension/src/content/App.tsx
@@ -0,0 +1,7 @@
+export default function Content() {
+  return (
+    <div>
+      <h1>Hello ISA EPFL!</h1>
+    </div>
+  )
+}
---
+++ chrome-extension/src/content/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import '../styles/index.css'
+import '../styles/loading-dots.css'
+
+function checkForElement(): Promise<HTMLElement | null> {
+  return new Promise((resolve) => {
+    const check = () => {
+      const targetElement = document.getElementById('BB308197177_300')
+      if (targetElement) {
+        console.log('Target element found!')
+        resolve(targetElement)
+      } else {
+        console.log('Element not found, checking again in 1 second...')
+        setTimeout(check, 1000)
+      }
+    }
+    check()
+  })
+}
+
+// Function to inject our React app
+function injectReactApp() {
+  checkForElement().then((targetElement) => {
+    console.log('injectReactApp', targetElement)
+    console.log('injectReactApp', document.querySelector('.blocinfobody'))
+    if (targetElement) {
+      const root = document.createElement('div')
+      root.id = 'extension-content-root'
+
+      // Replace the target element with our root element
+      targetElement.parentNode?.replaceChild(root, targetElement)
+
+      ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+      )
+    }
+  })
+}
+
+console.log('injectReactApp2')
+
+injectReactApp()
