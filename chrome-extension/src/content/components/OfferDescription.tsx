97e1b8798115c54e603ca27c1b9c8dcb081b6422
Pierre Véron <pierre.veron@outlook.fr>
2024-09-21 15:35:25 +0200

    fix: overflow of offer description

--- chrome-extension/src/content/components/OfferDescription.tsx
+++ chrome-extension/src/content/components/OfferDescription.tsx
@@ -49,7 +49,7 @@ export default function OfferDescription() {
   if (!asideOffer) return <div></div>
 
   return (
-    <div className="tw-pb-8">
+    <div className="tw-pb-8 tw-h-full tw-w-full tw-overflow-y-auto">
       <p className="tw-text-gray-500 tw-text-sm">
         {/* <span className="tw-font-medium">Offer number:</span>{" "} */}
         {asideOffer.number}
