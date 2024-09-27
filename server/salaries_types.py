5e8d06614cd6fc162fc85b2fb01ee8f349f4eda4
Pierre Véron <pierre.veron@outlook.fr>
2024-09-27 19:20:05 +0200

    refactor: remove server

--- server/salaries_types.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class SalariesDict(BaseModel):
-    salaries: dict[str, float | None] = Field(
-        description="dictionary of salaries with the original text as key"
-    )
