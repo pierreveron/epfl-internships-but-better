e1cad3f8a28ab7926b43ea4118689620a0c1d227
Pierre Véron <pierre.veron@outlook.fr>
2024-09-28 11:06:50 +0200

    feat: disable sorting by salary if not premium

--- chrome-extension/src/content/components/SortDropdown.tsx
+++ chrome-extension/src/content/components/SortDropdown.tsx
@@ -1,21 +1,32 @@
-import { Select } from '@mantine/core'
-import { IconArrowsSort } from '@tabler/icons-react'
+import { Group, Select, SelectProps } from '@mantine/core'
+import { IconArrowsSort, IconCheck, IconCrown } from '@tabler/icons-react'
 import { useContext } from 'react'
 import { SortContext } from '../contexts/SortContext'
+import { useUser } from '../hooks/useUser'
 
-const sortOptions = [
-  { value: 'creationDate-desc', label: 'Creation date (newest first)' },
-  { value: 'creationDate-asc', label: 'Creation date (oldest first)' },
-  { value: 'company-asc', label: 'Company (A-Z)' },
-  { value: 'company-desc', label: 'Company (Z-A)' },
-  { value: 'salary-desc', label: 'Salary (high to low)' },
-  { value: 'salary-asc', label: 'Salary (low to high)' },
-  { value: 'registered-desc', label: 'Number of candidates (high to low)' },
-  { value: 'registered-asc', label: 'Number of candidates (low to high)' },
-]
+const renderSelectOption: SelectProps['renderOption'] = ({ option, checked }) => (
+  <Group flex="1" gap="0.5rem" wrap="nowrap">
+    {checked && <IconCheck size={16} />}
+    <span className="tw-text-sm">{option.label}</span>
+    {option.disabled && <IconCrown size={16} style={{ marginInlineStart: 'auto' }} />}
+  </Group>
+)
 
 export default function SortDropdown() {
   const { sortStatus, setSortStatus } = useContext(SortContext)!
+  const { user } = useUser()
+  const isPremiumUser = user?.hasPremium ?? false
+
+  const sortOptions = [
+    { value: 'creationDate-desc', label: 'Creation date (newest first)' },
+    { value: 'creationDate-asc', label: 'Creation date (oldest first)' },
+    { value: 'company-asc', label: 'Company (A-Z)' },
+    { value: 'company-desc', label: 'Company (Z-A)' },
+    { value: 'salary-desc', label: 'Salary (high to low)', disabled: !isPremiumUser },
+    { value: 'salary-asc', label: 'Salary (low to high)', disabled: !isPremiumUser },
+    { value: 'registered-desc', label: 'Number of candidates (high to low)' },
+    { value: 'registered-asc', label: 'Number of candidates (low to high)' },
+  ]
 
   const handleChange = (value: string | null) => {
     if (value) {
@@ -30,6 +41,7 @@ export default function SortDropdown() {
       onChange={handleChange}
       data={sortOptions}
       leftSection={<IconArrowsSort />}
+      renderOption={renderSelectOption}
     />
   )
 }
